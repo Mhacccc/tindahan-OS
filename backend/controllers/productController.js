a91b09245d1da8a7540e25e8f5929eeaef227928
Mhacccc <zhamsoez@gmail.com>
2025-11-27 22:18:01 +0800

    Add controller to the backend

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,10 +1,55 @@
 const Product = require('../models/Product')
+const mongoose = require('mongoose')
 
-const addProduct = async (req,res) => {
-    const {name,price,stock,category} = req.body
+// Get all products
+const getProducts = async (req,res) => {
+
     try{
+        const product = await Product.find({})
+        res.status(200).send(product)
 
-        const productReq = await Product.create({name,price,stock,category})
+    }catch(error){
+        res.status(400).json({
+            error:error.message
+        })
+    }
+
+}
+
+
+// Get a product
+const getProduct = async (req,res)=>{
+    
+    const {id} = req.params
+
+    if(!mongoose.isValidObjectId(id)){
+        return res.status(400).json({
+            error: "Invalid ID"
+        })
+    }
+
+    try{
+        const product = await Product.findById(id)
+
+
+        if(!product){
+            return res.status(404).json({
+            error:"No such Product"})
+        }
+        return res.status(200).send(product)
+
+    }catch(error){
+        res.status(400).json({
+            error:error.message
+        })
+    }
+}
+
+// Add a product
+const addProduct = async (req,res) => {
+
+    try{
+        const productReq = await Product.create({...req.body})
         res.status(200).json(productReq)
 
     }catch(error){
@@ -12,13 +57,75 @@ const addProduct = async (req,res) => {
     }
 }
 
+// Delete a product
+const deleteProduct = async (req,res)=>{
 
-//get all products
-const getProducts = async (req,res) => {
+    const {id} = req.params
+
+    if(!mongoose.isValidObjectId(id)){
+        return res.status(404).json({
+            error: "Invalid ID"
+        })
+    }
+
+    try{
+        const productReq = await Product.findByIdAndDelete(id)
+        if(!productReq){
+            return res.status(404).json({
+                error: "No such Product"
+            })
+        }
+        res.json(productReq)
+        
+
+    }catch(error){
+        res.json(400).json({
+            error: error.message
+        })
+    }
+
+
+
+}
+
+// Update a product
+const updateProduct = async (req,res)=>{
+
+    const {id} = req.params
+
+    if(!mongoose.isValidObjectId(id)){
+        return res.status(404).json({
+            error: "Invalid ID"
+        })
+    }
+
+    try{
+
+        const productReq = await Product.findByIdAndUpdate(id,req.body,{
+            new: true,
+            runValidator: true
+        })
+
+        if(!productReq){
+            return res.status(404).json({
+                error: "No such product"
+            })
+        }
+        res.json(productReq)
+        
+    }catch(error){
+        res.status(404).json({
+                error: error.message
+            })
+    }
 
 }
 
 
 module.exports = {
-    addProduct
+    addProduct,
+    getProducts,
+    getProduct,
+    deleteProduct,
+    updateProduct,
 }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,23 +1,17 @@
 const express = require('express') ;
 
 const router = express.Router();
-const {addProduct} = require('../controllers/productController') 
+const {addProduct, getProduct, getProducts, updateProduct, deleteProduct} = require('../controllers/productController') 
 
-router.get('/',(req,res)=>{
-    res.json({message: "GET ALL PRODUCTS"})  
-})
-router.get('/:id',(req,res)=>{
-    res.json([{message: "GET SINGLE PRODUCT"}])  
-})
+router.get('/', getProducts)
+
+router.get('/:id', getProduct)
 
 router.post('/', addProduct)
 
-router.delete('/:id',(req,res)=>{
-    res.json({message: "DELETE A PRODUCT"})  
-})
-router.put('/:id',(req,res)=>{
-    res.json({message: "UPDATE A PRODUCT"})
-})
+router.delete('/:id', deleteProduct)
+
+router.patch('/:id', updateProduct)
 
 
 
