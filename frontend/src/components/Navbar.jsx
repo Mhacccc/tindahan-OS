ad95f5eebd6dbd918734453b3890c1ca77d5cdda
Mhacccc <zhamsoez@gmail.com>
2025-11-29 10:09:32 +0800

    Add navbar and home components

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar () {
+        
+    return (
+        <header>
+            <div className="Navbar">
+                <Link to="/">
+                    <h1>
+                        Tindahan
+                    </h1>
+                </Link>
+            </div>
+        </header>
+    )
+    
+
+}
